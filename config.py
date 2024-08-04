5e1b5fb699a7aa030932fc93d59796c09c94ddab
madman38 <160655444+madman38@users.noreply.github.com>
2024-08-05 00:23:24 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # only files with these extensions will be downloaded
-ALLOWED_FILE_TYPES = ['mp4', 'mp3', 'png', 'jpg', 'webp']
+ALLOWED_FILE_TYPES = ['mp4', 'mp3', 'png', 'jpg', 'webp', 'mov']
 
 # maximum allowed size for downloadable files in megabytes
 MAX_DOWNLOAD_SIZE_MB = 25
